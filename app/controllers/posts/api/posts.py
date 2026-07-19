90e68f7a5ef7868ca1102f13d9e656df5ef901f7
Martin Silungwe <martinsilungwe12@gmail.com>
2026-07-19 17:23:36 +0200

    working on post routes

--- app/controllers/posts/api/posts.py
+++ app/controllers/posts/api/posts.py
@@ -1,7 +1,38 @@
-from fastapi import APIRouter, HTTPException, status
-from app.schemas.post_schemas import PostResponse
+from fastapi import APIRouter, HTTPException, status, Depends
+from app.schemas.post_schemas import PostResponse, CreatePost
+from typing import Annotated
+from sqlalchemy.ext.asyncio.session import AsyncSession
+from app.core.database import app_session
+from app.services.post_services import PostServices
+from fastapi.responses import JSONResponse
+
 posts_router = APIRouter()
+post_services = PostServices()
 
 @posts_router.post("/", status_code=status.HTTP_201_CREATED, response_model=PostResponse)
-async def root():
-    return {"message": "hey mate from posts"}
+async def create_post(post_data: CreatePost, session: Annotated[AsyncSession, Depends(app_session)]):
+    user_exists = await post_services.check_if_user_exists(
+        post_data=post_data,
+        session=session
+    )
+    
+    if not user_exists:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User not found"
+        )
+    
+    new_post = await post_services.create_new_post(
+        post_data=post_data,
+        session=session
+    )
+    
+    return JSONResponse(
+        status_code=status.HTTP_201_CREATED,
+        content={
+            "message": "Post created successfully",
+            "new_post": {
+                
+            }
+        }
+    )
--- app/services/post_services.py
+++ app/services/post_services.py
@@ -1,19 +1,31 @@
 from app.schemas.post_schemas import CreatePost
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio.session import AsyncSession
-from app.models.users import User
 from app.models.posts import Post
 
 
 class PostServices:
     
+    async def check_if_user_exists(self, post_data: CreatePost, session: AsyncSession):
+        statement = await session.execute(
+            select(Post).where(Post.id == post_data.user_id)
+        )
+        
+        user_exists = statement.scalars().first()
+        
+        if user_exists:
+            return True
+        else:
+            return False
+        
     # we will create the post here 
     async def create_new_post(self, post_data: CreatePost, session: AsyncSession):
-        statement = await session.execute(
-            select(User).where(User.id == post_data.user_id)
+        user_exists = await self.check_if_user_exists(
+            post_data=post_data,
+            session = session
         )
-        user = statement.scalars().first()
-        if not user:
+        
+        if not user_exists:
             return None
         else:
             post_dict = post_data.model_dump()
@@ -22,4 +34,5 @@ class PostServices:
             await session.commit()
             await session.refresh(new_post)
             return new_post
+            
         
